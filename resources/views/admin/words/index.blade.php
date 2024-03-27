3e260aa9b6c3c3c9c99887e44584e145ffd888d6
Polvere243 <danielercoli@gmail.com>
2024-03-27 12:04:51 +0100

    added container

--- resources/views/admin/words/index.blade.php
+++ resources/views/admin/words/index.blade.php
@@ -1,33 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <table>
+        <tr>
+            <th>Titolo</th>
+            <th>Slug</th>
+            <th>Definizione</th>
+            <th>Azioni</th>
+        </tr>
+        @forelse($words as $word)
+        <tr>
+            <td>{{ $word->title }}</td>
+            <td>{{ $word->slug }}</td>
+            <td>{{ $word->definition }}</td>
+            <td>
+                <a href=""><i class="fas fa-eye"></i></a>
+                <a href=""><i class="fas fa-pencil"></i></a>
+                <form action="">
+                    <button>
+                        <i class="fas fa-trash-can"></i>
+                    </button>
+                </form>
+            </td>
+        </tr>
+        @empty
+        <h1>Non ci sono parole...</h1>
+        @endforelse
+        
+    </table>
+</div>
 
-<table>
-    <tr>
-        <th>Titolo</th>
-        <th>Slug</th>
-        <th>Definizione</th>
-        <th>Azioni</th>
-    </tr>
-    @forelse($words as $word)
-    <tr>
-        <td>{{ $word->title }}</td>
-        <td>{{ $word->slug }}</td>
-        <td>{{ $word->definition }}</td>
-        <td>
-            <a href=""><i class="fas fa-eye"></i></a>
-            <a href=""><i class="fas fa-pencil"></i></a>
-            <form action="">
-                <button>
-                    <i class="fas fa-trash-can"></i>
-                </button>
-            </form>
-        </td>
-    </tr>
-    @empty
-    <h1>Non ci sono parole...</h1>
-    @endforelse
-    
-</table>
 
 @endsection
