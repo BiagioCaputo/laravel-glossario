c2fe8897da52dc0a4d2397b9b7ba659ea989e5dc
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-05 16:21:35 +0200

    added link inputs to the forms with collapse

--- resources/views/includes/words/form.blade.php
+++ resources/views/includes/words/form.blade.php
@@ -3,8 +3,8 @@
     @method('PUT')
 @else
 <form action="{{route('admin.words.store')}}" method="POST">
-@endif
-@csrf
+    @endif
+    @csrf
     <div class="row align-items-center justify-content-start">
         {{-- Titolo --}}
         <div class="col-6">
@@ -39,6 +39,57 @@
                 @enderror
             </div>
         </div>
+        {{-- Links --}}
+        <p class="d-inline-flex gap-1">
+            <a class="btn btn-primary" data-bs-toggle="collapse" href="#link-1" role="button" aria-expanded="false" aria-controls="link-1"><i class="fas fa-plus"></i> Link</a>
+            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse" aria-expanded="false" aria-controls="link-1 link-2 link-3">Mostra tutti</button>
+        </p>
+          <div class="row">
+            <div class="col-12">
+              <div class="collapse multi-collapse" id="link-1">
+                <div class="row">
+                    <div class="col-6">
+                        <label for="label" class="form-label">Label</label>
+                        <input type="text" class="form-control" id="label" placeholder="Inserisci un nome del link...">
+                    </div>
+                    <div class="col-6">
+                        <label for="url" class="form-label">Url</label>
+                        <input type="url" class="form-control" id="url" placeholder="Inserisci un url del link...">
+                    </div>
+                </div>
+                <button class="btn btn-primary my-4" type="button" data-bs-toggle="collapse" data-bs-target="#link-2" aria-expanded="false" aria-controls="link-2">More <i class="fas fa-link"></i></button>
+              </div>
+            </div>
+            <div class="col-12">
+                <div class="collapse multi-collapse" id="link-2">
+                    <div class="row">
+                        <div class="col-6">
+                            <label for="label" class="form-label">Label</label>
+                            <input type="text" class="form-control" id="label" placeholder="Inserisci un nome del link...">
+                        </div>
+                        <div class="col-6">
+                            <label for="url" class="form-label">Url</label>
+                            <input type="url" class="form-control" id="url" placeholder="Inserisci un url del link...">
+                        </div>
+                    </div>
+                    <button class="btn btn-primary my-4" type="button" data-bs-toggle="collapse" data-bs-target="#link-3" aria-expanded="false" aria-controls="link-3">More <i class="fas fa-link"></i></button>
+                </div>
+            </div>
+            <div class="col-12">
+              <div class="collapse multi-collapse" id="link-3">
+                <div class="row">
+                    <div class="col-6">
+                        <label for="label" class="form-label">Label</label>
+                        <input type="text" class="form-control" id="label" placeholder="Inserisci un nome del link...">
+                    </div>
+                    <div class="col-6">
+                        <label for="url" class="form-label">Url</label>
+                        <input type="url" class="form-control" id="url" placeholder="Inserisci un url del link...">
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
     </div>
     <hr>
     {{-- Bottoni --}}
