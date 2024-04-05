70ec5de13108578b4af2426842b6322b08341ad2
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-05 17:00:26 +0200

    deleted actions show & create

--- app/Http/Controllers/Admin/LinkController.php
+++ app/Http/Controllers/Admin/LinkController.php
@@ -19,25 +19,9 @@ class LinkController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
     {
         //
     }
