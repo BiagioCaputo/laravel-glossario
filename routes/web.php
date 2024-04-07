ad6f97f257657deefb171d44fcfd44bbf9f7d704
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-04-07 22:16:33 +0200

    added routes links & tags trash

--- routes/web.php
+++ routes/web.php
@@ -29,13 +29,13 @@ Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function () {
     //Rotta admin home
     Route::get('/', AdminHomeController::class)->name('home');
 
-    //trash
+    //Rotte admin words trash
     Route::get('/words/trash', [WordController::class, 'trash'])->name('words.trash');
     Route::patch('/words/{word}/restore', [WordController::class, 'restore'])->name('words.restore');
     Route::delete('/words/{word}/drop', [WordController::class, 'drop'])->name('words.drop');
 
 
-    //Rotte admin post
+    //Rotte admin words
     Route::get('/words', [WordController::class, 'index'])->name('words.index');
     Route::get('/words/create', [WordController::class, 'create'])->name('words.create');
     Route::get('/words/{word}', [WordController::class, 'show'])->name('words.show')->withTrashed();
@@ -44,19 +44,28 @@ Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function () {
     Route::put('/words/{word}', [WordController::class, 'update'])->name('words.update')->withTrashed();
     Route::delete('/words/{word}', [WordController::class, 'destroy'])->name('words.destroy');
 
+    //Rotte admin links trash
+    Route::get('/links/trash', [LinkController::class, 'trash'])->name('links.trash');
+    Route::patch('/links/{link}/restore', [LinkController::class, 'restore'])->name('links.restore');
+    Route::delete('/links/{link}/drop', [LinkController::class, 'drop'])->name('links.drop');
 
-    // Rotta admin link
+    //Rotte admin links
     Route::get('/links', [LinkController::class, 'index'])->name('links.index');
     Route::post('/links', [LinkController::class, 'store'])->name('links.store');
-    Route::get('/links/{link}/edit', [LinkController::class, 'edit'])->name('links.edit');
-    Route::put('/links/{link}', [LinkController::class, 'update'])->name('links.update');
+    Route::get('/links/{link}/edit', [LinkController::class, 'edit'])->name('links.edit')->withTrashed();
+    Route::put('/links/{link}', [LinkController::class, 'update'])->name('links.update')->withTrashed();
     Route::delete('/links/{link}', [LinkController::class, 'destroy'])->name('links.destroy');
 
 
+    //Rotte admin tags trash
+    Route::get('/tags/trash', [TagController::class, 'trash'])->name('tags.trash');
+    Route::patch('/tags/{tag}/restore', [TagController::class, 'restore'])->name('tags.restore');
+    Route::delete('/tags/{tag}/drop', [TagController::class, 'drop'])->name('tags.drop');
+
     //Rotte admin tags
     Route::get('/tags', [TagController::class, 'index'])->name('tags.index');
-    Route::get('/tags/{tag}/edit', [TagController::class, 'edit'])->name('tags.edit');
-    Route::put('/tags/{tag}', [TagController::class, 'update'])->name('tags.update');
+    Route::get('/tags/{tag}/edit', [TagController::class, 'edit'])->name('tags.edit')->withTrashed();
+    Route::put('/tags/{tag}', [TagController::class, 'update'])->name('tags.update')->withTrashed();
     Route::delete('/tags/{tag}', [TagController::class, 'destroy'])->name('tags.destroy');
 });
 
