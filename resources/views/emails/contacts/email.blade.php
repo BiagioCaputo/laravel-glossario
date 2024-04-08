29d3338315292d19c7356ce6cf896a948239ebf4
Polvere243 <danielercoli@gmail.com>
2024-04-08 13:50:24 +0200

    created a controller, a mailable, a blade for the email and a route

---
+++ resources/views/emails/contacts/email.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ env('APP_NAME') }}</title>
+    <style>
+        body{
+            text-align: center;
+            padding: 50px;
+
+        }
+
+    </style>
+</head>
+<body>
+    <p>È stato inviato un messaggio.</p>
+</body>
+</html>
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\WordController;
+use App\Http\Controllers\Api\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,9 +20,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// rotte per le chiamate API
 Route::get('/words', [WordController::class, 'index']);
 Route::get('/words/{slug}', [WordController::class, 'show']);
 Route::post('/words', [WordController::class, 'store']);
 Route::delete('/words/{word}', [WordController::class, 'destroy']);
 Route::put('/words/{word}', [WordController::class, 'update']);
+
+
+// rotta per i messaggi
+Route::post('/contact-message', [ContactController::class, 'message']);
